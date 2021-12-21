606a48ca42f62274294abfe07865205f4422bd57
Pablo Niklas <pablo.niklas@gmail.com>
2021-12-21 20:22:18 -0300

    New command: search

--- classes/Memory.py
+++ classes/Memory.py
@@ -4,9 +4,9 @@ from multipledispatch import dispatch
 class Memory:
 
     def __init__(self, pages=1):
-
         self.pages = pages
-        self.page_cursor = 0
+        self._offset_cursor = 0
+        self.active_page = 49152  # Like the old one C000
         self._offsets = 65536  # 64K for page.
         self.memory = [[0b00000000] * self._offsets] * self.pages
 
@@ -15,13 +15,12 @@ class Memory:
 
     @dispatch(int, int)
     def peek(self, page, address):
-
         if int(page) < 0 or int(page) >= len(self.memory) or \
                 int(address) < 0 or int(page) >= self._offsets:
             print("Memory.peek(): Value error.")
             return -1
         else:
-            self.page_cursor = page
+            self.active_page = page
             return self.memory[int(page)][int(address)]
 
     @dispatch(str, str)
@@ -30,26 +29,24 @@ class Memory:
 
     @dispatch(int, int, int)
     def poke(self, page, address, value):
-
         if value < 0 or value > 255 or \
                 page < 0 or page >= len(self.memory) or \
                 address > self._offsets or address < 0:
             print("Memory.poke(): Value error.")
             return -1
         else:
-            self.page_cursor = page
+            self.active_page = page
             self.memory[page][address] = value
 
     @dispatch(int, int)
     def display(self, addrb, addrn):
-
-        page = self.page_cursor
+        page = self.active_page
         bytes_per_row = int("F", 16)
         pointer = 0
         ascvisual = ""
 
         if addrn - addrb < bytes_per_row:  # One single row
-            print(f"{'%04X' % self.page_cursor}:{'%04X' % (pointer + addrb)} ", end="", flush=True)
+            print(f"{'%04X' % self.active_page}:{'%04X' % (pointer + addrb)} ", end="", flush=True)
             for address in range(addrb, addrn):
                 byte = self.peek(page, pointer + addrb)
                 peek = "%02X" % byte
@@ -58,11 +55,11 @@ class Memory:
 
             print(" " * ((bytes_per_row - pointer) * 3) + ascvisual)
         else:  # two or more rows
-            while (pointer + addrb < addrn):
+            while pointer + addrb < addrn:
                 if pointer % bytes_per_row == 0:
                     print(" " * ((bytes_per_row - pointer) * 3) + ascvisual)
                     ascvisual = ""
-                    print(f"{'%04X' % self.page_cursor}:{'%04X' % (pointer + addrb)} ", end="", flush=True)
+                    print(f"{'%04X' % self.active_page}:{'%04X' % (pointer + addrb)} ", end="", flush=True)
 
                 byte = self.peek(page, pointer + addrb)
                 peek = "%02X" % byte
@@ -76,10 +73,32 @@ class Memory:
     def fill(self, start, end, pattern):
         cursor = 0
         for idx in range(start, end):
-            self.poke(self.page_cursor, idx, ord(pattern[cursor]))
+            self.poke(self.active_page, idx, ord(pattern[cursor]))
             cursor += 1
             if cursor > len(pattern) - 1:
                 cursor = 0
+
+    @dispatch(int, str)
+    def search(self, start, pattern):
+        found_list = []
+        pointer = start
+        while pointer < self._offsets:
+            idx = 0
+
+            # Did I find the first char of the pattern? If so, let's search for the rest
+            if self.peek(self.active_page, pointer) == ord(pattern[idx]):
+                pointer_aux = pointer
+                while idx < len(pattern) and pointer_aux < self._offsets and \
+                        self.peek(self.active_page, pointer_aux) == ord(pattern[idx]):
+                    idx += 1
+                    pointer_aux += 1
+
+                if pointer_aux - pointer == len(pattern):
+                    found_list.append(f"{'%04X' % self.active_page}:{'%04X' % pointer}")
+
+            pointer += 1
+
+        return found_list
 
     @dispatch(int, int, str)
     def load_into(self, page, start, text):
--- readme.md
+++ readme.md
@@ -6,8 +6,8 @@ An x86 old-debug-like program.
 
 _still an ongoing project_
 
-The main goal of this project is to provide 
-an educational and introductory interface to assmebler, x86 flavor in particular.
+The main goal of this project is to provide an educational and introductory interface to assmebler, x86 flavor in
+particular.
 
 The user can interact directly with the memory the program presents.
 
@@ -23,7 +23,9 @@ This modes are available at this time;
 ### Command mode
 
 * **d** xx yy: display memory from _xx_ to _yy_
-* **f** xx yy pp: fill memory from _xx_ to _yy_ with pattern _pp_. Without the _pp_ arguments, just blank the memory in the provided range.
+* **s** xx pp: Searches within a page, from the address _xx_ for a pattern _pp_
+* **f** xx yy pp: fill memory from _xx_ to _yy_ with pattern _pp_. Without the _pp_ arguments, just blank the memory in
+  the provided range.
 * **demo**: load a predefined string into the first bytes of its memory.
 * **sp** xx: set default memory page to _sp_.
 * **q** or **quit**: to quit this program.
