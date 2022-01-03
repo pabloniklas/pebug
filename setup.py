8080e19cf049679c651707a7d46930ef7313d7aa
Pablo Niklas <pablo.niklas@gmail.com>
2022-01-03 15:37:33 -0300

    Initial Load.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pebug',
+    version='0.5',
+    packages=[''],
+    url='https://github.com/pabloniklas/pebug',
+    license='BSD',
+    author='Pablo Niklas aka ttys9',
+    author_email='pablo.niklas@gmail.com',
+    description='An x86 old-debug-like program.'
+)
