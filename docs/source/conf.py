3613af519fa79913251241295c875a30e534048b
Pablo Niklas <pablo.niklas@gmail.com>
2022-01-03 15:53:08 -0300

    Theme.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,10 +105,10 @@ exclude_patterns = ['_build', ]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinxdoc'
 # html_theme = 'classic'
-#html_theme = 'bizstyle'
+# html_theme = 'bizstyle'
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
