039d75a2581b83ba2579ff08b35b7d19025e1f8d
Pablo Niklas <pablo.niklas@gmail.com>
2022-04-11 20:24:26 -0300

    Fix

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,5 @@
 from .asm8086Listener import asm8086Listener
 from .asm8086Parser import asm8086Parser
-from .asm8086Machine import *
+from .Cpu import Cpu
+from .Disk import Disk
+from .Memory import Memory
