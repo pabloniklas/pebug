349eb8c04eaf72b2981bc308d0aacebc14de0a39
Pablo Niklas <pablo.niklas@gmail.com>
2022-07-15 08:03:36 -0300

    assemble _alpha_

--- opcode_dict/txt2pydict.py
+++ opcode_dict/txt2pydict.py
@@ -2,74 +2,87 @@ import re
 
 f = open("opcode.txt")
 print("self._opcode = {")
+line32 = False
 
 oldcase = ["", "", "", "", ""]
 
 for line in f:
-    print("    # LINE ==> " + line[:51])
+    print("    # SOURCE LINE ==> " + line[:51])
 
-    opcode = line[:7].strip()
-    mnemonic = line[7:16].strip()
-    operands = line[16:33].strip()
-    flags = line[34:51].strip()
+    # Only 16 bits (or less =)
+    if re.search(r'(32)|(E[ABCD]X)|E(SBI)P',
+                 line.strip(), re.MULTILINE) is None and line[16:33].strip() != "":
 
-    re.sub("[F,]", "", flags)
+        line32 = False
+        opcode = line[:7].strip()
+        mnemonic = line[7:16].strip()
+        operands = line[16:33].strip()
+        flags = line[34:51].strip()
 
-    count = 0
-    oldcase[0] = "(\\s+)"
+        re.sub("[F,]", "", flags)
 
-    if opcode == "":
-        #print("######## LINE EMPTY #######")
-        mnemonic = oldcase[2]
-        opcode = oldcase[1]
-        flags = oldcase[4]
-        operands = oldcase[3]
+        count = 0
+        oldcase[0] = "(\\s+)"
 
-    for oper in operands.split(","):
-        if re.match(r"none", oper):
-            pass
-        elif re.match(r"(mem8|rel8)", oper):
-            oldcase[0] = oldcase[0] + "[0-9a-f]{2}"
-        elif re.match(r"(mem16|rel16)", oper):
-            oldcase[0] = oldcase[0] + "[0-9a-f]{4}"
-        elif re.match(r"(mem32|rel32)", oper):
-            oldcase[0] = oldcase[0] + "[0-9a-f]{8}"
-        elif re.match(r"reg8", oper):
-            oldcase[0] = oldcase[0] + "[abcd][hl]"
-        elif re.match(r"reg16", oper):
-            oldcase[0] = oldcase[0] + "[abcd]x"
-        elif re.match(r"reg32", oper):
-            oldcase[0] = oldcase[0] + "e[abcd]x"
-        elif re.match(r"imm8", oper):
-            oldcase[0] = oldcase[0] + "al"
-        elif re.match(r"imm16", oper):
-            oldcase[0] = oldcase[0] + "ax"
+        if opcode == "" and mnemonic != oldcase[2]:  # Empty line, recover the values of previusly filled line
+            mnemonic = oldcase[2]
+            opcode = oldcase[1]
+            flags = oldcase[4]
+            operands = oldcase[3]
 
-        # Saving the values.
-        if opcode != "":
-            oldcase[2] = mnemonic
-            oldcase[1] = opcode
-            oldcase[3] = operands
-            oldcase[4] = flags
+        for oper in operands.split(","):
+            if re.match(r"none", oper):
+                pass
+            elif re.match(r"(mem8|rel8)", oper):
+                oldcase[0] += "[0-9a-f]{2}"
+            elif re.match(r"(mem16|rel16)", oper):
+                oldcase[0] += "[0-9a-f]{4}"
+            elif re.match(r"reg8", oper):
+                oldcase[0] += "[abcd][hl]"
+            elif re.match(r"reg16", oper):
+                oldcase[0] += "[abcd]x"
+            elif re.match(r"imm8", oper):
+                oldcase[0] += "al"
+            elif re.match(r"imm16", oper):
+                oldcase[0] += "ax"
+            elif re.match(r"\b([CDEFGS]S)\b|\b([SD]I)\b|\b([ABCDE]X)\b|\b([ABCD][HL])\b|\b([SBI]P)\b",
+                          oper, re.IGNORECASE):
+                oldcase[0] += f"{oper}"
 
-        # At least one space after the comma. Just my OCD :)
-        count += 1
+            # Saving the values.
+            if opcode != "":
+                oldcase[1] = opcode
+                oldcase[2] = mnemonic
+                oldcase[3] = operands
+                oldcase[4] = flags
 
-    if count < len(operands):
-        oldcase[0] = oldcase[0] + ",(\\s+)"
+            # At least one space after the comma. Just my OCD :)
+            count += 1
 
-    # Create the opcodes list
-    opcodes = "[ "
-    for opc in opcode.split(" "):
-        opcodes += f"int(0x{opc}),"
+            if count < len(operands) - 2:
+                oldcase[0] = oldcase[0] + ",(\\s+)"
+            else:
+                oldcase[0] = oldcase[0] + "(\\s+)"
 
-    re.sub(',$', '', opcodes)
-    opcodes += " ]"
+        # Create the opcodes list
+        opcodes_list_count = 0
+        opcodes = "[ "
+        for opc in opcode.split(" "):
+            opcodes_list_count += 1
+            if opcodes_list_count < len(opcode.split(" ")):
+                opcodes += f"int(0x{opc}),"
+            else:
+                opcodes += f"int(0x{opc})"
 
-    print("    r\"^(\\s*)" + mnemonic + oldcase[0] + "\": {")
-    print("        \"mnemonic\": \"" + mnemonic + " " + operands + "\",")
-    print("        \"opcodes\": " + opcodes + ",")
-    print("        \"flags\": \"" + flags + "\"},")
+        opcodes += " ]"
+
+        if line32 is False:
+            print("    r\"^(\\s*)" + mnemonic + oldcase[0] + "\": {")
+            print("        \"mnemonic\": \"" + mnemonic + " " + operands + "\",")
+            print("        \"opcodes\": " + opcodes + ",")
+            print("        \"flags\": \"" + flags + "\"},")
+    else:
+        line32 = True
 
 print("}")
 f.close()
