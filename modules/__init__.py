916f95395efcbc24a53c97b01e4fa89bb2026ac6
Pablo Niklas <pablo.niklas@gmail.com>
2023-05-06 20:17:52 -0300

    Implementando nltk.

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,3 @@
-from .asm8086Listener import asm8086Listener
-from .asm8086Parser import asm8086Parser
 from .Cpu import Cpu
 from .Disk import Disk
 from .Memory import Memory
--- opcode_dict/txt2pydict.py
+++ opcode_dict/txt2pydict.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import re
 
 f = open("opcode.txt")
